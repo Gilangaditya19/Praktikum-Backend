3e01f4a85059d1639c671d996062d73090d31ba7
Gilangaditya19 <langzaditya21@gmail.com>
2023-12-21 20:27:30 +0700

    Menambahkan hasil tugas pertemuan ke 11 backend 2023

---
+++ praktikum11/backend-2023-pertemuan-11/task/data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+let students = ["Gilang Aditya", "Aditya Gilang", "Gilang keren"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
